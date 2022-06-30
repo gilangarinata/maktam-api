3ad2cfc41e0dae8214e83513f17a3d04a05e6be0
Arinata Gilang Prilian (GROW/PAX-AS) <arg1jk@bosch.com>
2022-06-30 16:20:22 +0700

    first commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,15 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
+	// 'hostname' => 'localhost',
+	// 'username' => 'root',
+	// 'password' => '',
+	// 'database' => 'maktam',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'maktam',
+	'username' => 'u151422786_root',
+	'password' => 'Sukses2021',
+	'database' => 'u151422786_pos_susumaktam',
+
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
